7d569cb756d0e2efce53fa7ce284322e9e5ba5db
Maria Shchurova <stirb.nicht.vor.mir@gmail.com>
2022-05-08 15:02:53 +0200

    seeThroughWalls shader

---
+++ Assets/Scripts/CircleSync.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CircleSync : MonoBehaviour
+{
+    public static int PositionID = Shader.PropertyToID("_Position");
+    public static int SizeID = Shader.PropertyToID("_Size");
+
+    public Material WallMaterial;
+    public Camera Camera;
+    public LayerMask CollisionMask;
+    void Update()
+    {
+        if (Camera == null)
+            return;
+
+        var dir = Camera.transform.position - transform.position;
+        var ray = new Ray(transform.position, dir.normalized);
+
+        if (Physics.Raycast(ray, 3000, CollisionMask))
+            WallMaterial.SetFloat(SizeID, 1);
+        else
+            WallMaterial.SetFloat(SizeID, 0);
+
+        var view = Camera.WorldToViewportPoint(transform.position);
+        WallMaterial.SetVector(PositionID, view);
+    }
+}

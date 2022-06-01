3c53ed50a75f77b11e30427f5e13ad99bee5b6f6
Sophie Lehmann <sophie.lehmann@ue-germany.de>
2022-06-01 20:56:50 +0100

    caustics moving light
    seb helped with coding

---
+++ Assets/CookieSwitcher.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieSwitcher : MonoBehaviour
+{
+public Light spotLight;
+public Texture[] textures;
+public Texture mainTex;
+int i = 0;
+ 
+
+    void Update()
+    {
+
+        mainTex = textures[i];
+spotLight.cookie = mainTex;
+i++;
+if(i>textures.Length-1) i=0;
+    }
+}

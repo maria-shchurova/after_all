4a5f878567f7a527ec40ed71604e872fd7508eb9
Maria Shchurova <stirb.nicht.vor.mir@gmail.com>
2022-06-18 23:49:44 +0200

    mirror event finished

--- Assets/Scripts/BathroomManager.cs
+++ Assets/Scripts/BathroomManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,12 +10,20 @@ public class BathroomManager : MonoBehaviour
     [SerializeField] GameObject BathtubBadDecal;
     [SerializeField] GameObject BathtubYellowLight;
     [SerializeField] GameObject BathHappyObject;
+    [SerializeField] Material NewShirtMaterial;
+    [SerializeField] SkinnedMeshRenderer CharacterRenderer;
     void Start()
     {
         Messenger.AddListener("MirrorCrack", MirrorCrack);
         Messenger.AddListener("Smile", Smile);
         Messenger.AddListener("Whale", BathtubBad);
         Messenger.AddListener("BubblyBath", HappyBath);
+        Messenger.AddListener("Change shirt", Change);
+    }
+
+    private void Change()
+    {
+        CharacterRenderer.material = NewShirtMaterial;
     }
 
     void MirrorCrack()

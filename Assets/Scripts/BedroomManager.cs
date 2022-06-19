c82bd5f16e3b1b3ee76c77a5b7c2938380508d37
Maria Shchurova <stirb.nicht.vor.mir@gmail.com>
2022-06-19 20:29:17 +0200

    drugs -  bad option  done

--- Assets/Scripts/BedroomManager.cs
+++ Assets/Scripts/BedroomManager.cs
@@ -1,5 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 using UnityEngine;
 
 public class BedroomManager : MonoBehaviour
@@ -11,10 +13,25 @@ public class BedroomManager : MonoBehaviour
     [SerializeField] AudioClip happyBassSound;
     [SerializeField] AudioClip breakingBassSound;
     [SerializeField] AudioSource Bass;
+
+    [SerializeField] Volume m_Volume;
     void Start()
     {
         Messenger.AddListener("breakBass", BreakBass);
         Messenger.AddListener("playSound", HappyBass);
+
+        Messenger.AddListener("Noise", NoiseOn);
+    }
+
+    private void NoiseOn()
+    {
+        FilmGrain grain;
+        ChromaticAberration ChA;
+        m_Volume.profile.TryGet(out grain);
+        m_Volume.profile.TryGet(out ChA);
+
+        grain.active = true;
+        ChA.active = true;
     }
 
     // Update is called once per frame

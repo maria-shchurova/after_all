8852252e5b5d67c3e85a6ef96afaf9b2dacd1300
Sophie Lehmann <sophie.lehmann@ue-germany.de>
2022-07-04 18:21:11 +0100

    Intro done

--- Assets/IntroScript.cs
+++ Assets/IntroScript.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class IntroScript : MonoBehaviour
 {
+    [SerializeField] AudioClip Intro1;
+    [SerializeField] AudioClip Intro2;
+
+    AudioSource myAudio;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         
+        Messenger.AddListener("Intro1",Open);
+        Messenger.AddListener("Intro2", Slamming);
+
+        myAudio = GetComponent<AudioSource>();
+
+        Invoke("Open", 50f);
+        Invoke("Slamming", 53f);
     }
 
     // Update is called once per frame
@@ -15,4 +28,23 @@ public class IntroScript : MonoBehaviour
     {
         
     }
+
+    void Open()
+    {
+        myAudio.clip = Intro1;
+        myAudio.Play();
+    }
+
+
+    void Slamming()
+    {
+        myAudio.clip = Intro2;
+        myAudio.Play();
+    }
+
+    void GetRid()
+    {
+        Destroy(gameObject, 65f);
+    }
+
 }

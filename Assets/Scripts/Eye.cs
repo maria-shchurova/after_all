9ba214983fdf0e0d62f1640d70243c0da384d851
Sophie Lehmann <sophie.lehmann@ue-germany.de>
2022-07-11 18:01:19 +0100

    moving Eye WIP

--- Assets/Scripts/Eye.cs
+++ Assets/Scripts/Eye.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class Eye : MonoBehaviour
 {
-    [SerializeField] GameObject Canvas;
+    //[SerializeField] GameObject Canvas;
+    [SerializeField] GameObject Eyeball;
+
+    private Transform target;
+    //public Vector3 turn;
+
+    void Start()
+    {
+        target.position = Input.mousePosition;
+    }
 
     void Update()
     {
-        //Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        //float angle = (Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg) - 90f;
+        //Vector3 turn = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //float angle = (Mathf.Atan2(-turn.y, turn.x) * Mathf.Rad2Deg) - 90f;
         //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        //transform.position = Canvas.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f,0f,1f);
+        //transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f,0f,1f);
+
+        //Eyeball.transform.rotation = Input.mousePosition;
+
+        transform.LookAt(target);
+
     }
+       
+
+  
 }

9094334ddb33a0556ee59bbf6f44289aa1e1451e
Sophie Lehmann <sophie.lehmann@ue-germany.de>
2022-07-11 16:44:20 +0100

    UI SPrites and Credits Update

---
+++ Assets/Scripts/Eye.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour
+{
+    [SerializeField] GameObject Canvas;
+
+    void Update()
+    {
+        //Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //float angle = (Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg) - 90f;
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        //transform.position = Canvas.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f,0f,1f);
+    }
+}
